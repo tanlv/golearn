4197710738b1f07b580301a2eb9cc206a4506ae6
tanlv <vantanpro2008@gmail.com>
2022-07-24 20:56:44 +0700

    Update new mini game

--- main.go
+++ main.go
@@ -106,20 +106,18 @@ func chooseSecretPerson() {
 
 func showMultiTable() {
 	var number1, number2 int
+	var input1, input2 string
 	var err1, err2 error
 	// Người dùng nhập vào bảng cửu chương muốn xem
 	// Người dùng nhập vào khoảng ranger muốn in
 	// Chương trình in ra từng dòng kết quả
 	for {
 		fmt.Println("Nhập \"exit\" nếu muốn thoát chương trình")
-		// Input nhập bản cửu chương cần xem
-		scanner1 := bufio.NewScanner(os.Stdin)
 		// check giá trị đầu vào
 		for {
 			// lấy giá trị
-			fmt.Printf("Nhập vào bảng cửu chương muốn xem: ")
-			scanner1.Scan()
-			input1 := scanner1.Text()
+			fmt.Print("Nhập vào bảng cửu chương muốn xem: ")
+			fmt.Scan(&input1, &err1)
 
 			if input1 == "" {
 				continue
@@ -142,14 +140,10 @@ func showMultiTable() {
 
 		}
 
-		// Input nhập ranger cần xem
-		scanner2 := bufio.NewScanner(os.Stdin)
-
 		// check giá trị đầu vào
 		for {
-			fmt.Printf("Nhập vào khoảng ranger của bảng cửu chương: ")
-			scanner2.Scan()
-			input2 := scanner2.Text()
+			fmt.Print("Nhập vào khoảng ranger của bảng cửu chương: ")
+			fmt.Scan(&input2)
 			if input2 == "" {
 				continue
 			}
@@ -174,9 +168,8 @@ func showMultiTable() {
 		fmt.Printf("Bảng Cửu Chương: %d\n", number1)
 		inc := 0
 		for {
-			res := number1 * inc
 			// vòng lặp print bảng cửu chương
-			fmt.Printf("%d x %d = %d\n", number1, inc, res)
+			fmt.Printf("%d x %d = %d\n", number1, inc, number1*inc)
 			inc++
 			if inc > number2 {
 				break
@@ -185,6 +178,53 @@ func showMultiTable() {
 
 		fmt.Println("=================================")
 	}
+}
+
+func randomStep() int {
+	source := rand.NewSource(time.Now().UnixMilli()) // Can tra google ve viec random trong Go
+	r := rand.New(source)
+	numberRandom := r.Intn(10)
+	return numberRandom
+}
+
+func runingMan() {
+	var user1, user2 string
+	var total_step1, total_step2, round int
+
+	fmt.Print("Nhập tên người chơi 1: ")
+	fmt.Scan(&user1)
+	fmt.Print("Nhập tên người chơi 2: ")
+	fmt.Scan(&user2)
+	fmt.Print("Nhập số vòng đua: ")
+	fmt.Scan(&round)
+
+	fmt.Println("CUỘC ĐUA BẮT ĐẦU")
+
+	for x := 1; x <= round; x++ {
+		fmt.Printf("Vòng đua thứ %d \n", x)
+		step1 := randomStep()
+		// Delay
+		time.Sleep(2 * time.Second)
+
+		step2 := randomStep()
+		fmt.Printf("Người chơi %s đi được %d bước\n", user1, step1)
+		fmt.Printf("Người chơi %s đi được %d bước\n", user2, step2)
+		total_step1 = total_step1 + step1
+		total_step2 = total_step2 + step2
+		fmt.Printf("Tổng kết: \n %s đi được %d bước \n %s đi được %d bước\n ===========================\n", user1, total_step1, user2, total_step2)
+	}
+
+	var userWin string
+	var totalWin int
+	if total_step1 > total_step2 {
+		userWin = user1
+		totalWin = total_step1
+	} else {
+		userWin = user2
+		totalWin = total_step2
+	}
+
+	fmt.Printf("NGƯỜI CHƠI %s ĐÃ CHIẾN THẮNG VỚI TỔNG SỐ BƯỚC LÀ %d", userWin, totalWin)
 }
 
 func main() {
@@ -200,6 +240,9 @@ func main() {
 	//fmt.Println("Game \"Chọn Người bí mật\"")
 	//chooseSecretPerson()
 	// Bảng cửu chương
-	showMultiTable()
+	//showMultiTable()
+
+	// Game 3 Chạy đua
+	runingMan()
 
 }
